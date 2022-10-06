e1f0468134a88441386183098b0a2790676e2067
MarcoSpinaBZZ <spinam@bzz.ch>
2022-10-06 16:39:46 +0200

    neuer html & js datei für das hinzufügen neuer Tasks.

---
+++ bearbeiten.js
@@ -0,0 +1,25 @@
+function createTask() {
+    const addInput = document.getElementById("addInput")
+    const task = { title: addInput.value }
+
+    fetch("http://localhost:3000/tasks", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(task)
+    }).then((response) => response.json())
+        .then((data) => {
+            console.log("Succes:", data)
+        })
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("add");
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        createTask();
+    })
+
+});
